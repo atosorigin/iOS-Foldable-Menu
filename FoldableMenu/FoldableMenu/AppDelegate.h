29575d5a830266ce002fc372a9a97676bcb0e466
Mike Williams <mike.williams@atos.net>
2013-01-07 14:06:20 +0000

    creating project structure #1

---
+++ FoldableMenu/FoldableMenu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FoldableMenu
+//
+//  Created by Mike Williams on 07/01/2013.
+//  Copyright (c) 2013 Atos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FoldableMenu/FoldableMenu/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FoldableMenu
+//
+//  Created by Mike Williams on 07/01/2013.
+//  Copyright (c) 2013 Atos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
