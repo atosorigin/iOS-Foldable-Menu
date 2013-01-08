066deb1d1382972baf07716fe5e5b20e4cd42b16
Peter Brock <peterbrock@unknown60334b27ad57.gateway.2wire.net>
2013-01-08 11:32:52 +0000

    Updating implementation

--- FoldableMenu/FoldableMenu/ViewController.h
+++ FoldableMenu/FoldableMenu/ViewController.h
@@ -7,7 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import "MultiFoldView.h"
+#import "PaperFoldView.h"
 
 @interface ViewController : UIViewController
 
+@property (nonatomic, strong) PaperFoldView *paperFoldView;
+@property (nonatomic, strong) MKMapView *mapView;
+@property (nonatomic, strong) UIView *topView;
+@property (strong, nonatomic) IBOutlet UIView *containerView;
+
+- (IBAction)testClicked:(id)sender;
 @end
